391dc3d23123f0638396de85a1112d39ba7da1e2
thepotla <36852419+thepotla@users.noreply.github.com>
2019-04-30 01:09:20 -0700

    original source code

--- source code/fish.java
+++ source code/fish.java
@@ -1,10 +1,11 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Fish swim around the screen while the player (shark) tries to eat them. 
+ * Fish swim around the screen while the player (shark) tries to eat them. When 8 fish are eaten the 
+ * game is won
  * @version (a version number or a date)
  */
-public class Fish extends Objects
+public class fish extends Actor
 {
     /**
      * Act - do whatever the fish wants to do. This method is called whenever
@@ -12,21 +13,24 @@ public class Fish extends Objects
      */
     public void act() 
     {
+
         randomTurn();
-        move(1) ;
+        move(3) ;
         turnAtEdge();
     }
-	
     public void turnAtEdge()
     {
         if ( isAtEdge() )
-          turn(8);
+        {
+            turn(17);
+        }
     } 
-	
     public void randomTurn()
     {
-        if ( Greenfoot.getRandomNumber(800) < 10)
-          turn( Greenfoot.getRandomNumber(100)-45 ); 
+        if ( Greenfoot.getRandomNumber(100) < 10)
+        {
+            turn( Greenfoot.getRandomNumber(90)-45 ); 
+        }
     }
 }    
 
