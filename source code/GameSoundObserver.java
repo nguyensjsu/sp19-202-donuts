ac93690c6cc2fc007659bde65bc9c0766ae349fa
thepotla <36852419+thepotla@users.noreply.github.com>
2019-04-30 16:38:05 -0700

    sound effects for the game

---
+++ source code/GameSoundObserver.java
@@ -0,0 +1,28 @@
+import greenfoot.*;
+
+public class GameSoundObserver implements Observer
+{
+    
+    public GameSoundObserver()
+    {
+    }
+    
+    public void update(String item, GameMessage msg )
+    {
+        
+        if(item=="fish")
+        {
+            Greenfoot.playSound("bite.wav");
+        }
+        else if(item=="shark" ){
+            Greenfoot.playSound("au.wav");
+            Greenfoot.playSound("hawk_screeching.wav");
+        }
+       
+        
+        
+    }
+        
+        
+        
+}
